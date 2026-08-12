2aa88bdd701e9bc57319d3f6c6c2935a1e7af773
yinghuochong <yinghuochong@example.com>
2026-08-12 18:15:27 +0800

    Nvme 缓冲节省

---
+++ run_service.bat
@@ -0,0 +1,8 @@
+@echo off
+chcp 65001 >nul
+
+cd /d "%~dp0"
+
+rd /s /q "config\data" >nul 2>&1
+
+start "PaperBreakEdgeService" cmd /c "chcp 65001 >nul & %~dp0out\build\local-windows-vs2026-debug\src\service\Debug\PaperBreakEdgeService.exe --console --config %~dp0config\default-config.json"
